62b0c51320fea3106715c015a06b021826d07e4d
Eduardo Lima <dlima78@hotmail.com>
2020-11-11 10:54:42 -0300

    chore: cofig sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,29 @@ module.exports = {
     fileName: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          moduels: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
   },
   devServer: {
     contentBase: './public',
@@ -21,8 +40,8 @@ module.exports = {
     historyApiFallback: true
   },
   externals: {
-    react:'React',
-    'react-dom':'ReactDOM'
+    react: 'React',
+    'react-dom': 'ReactDOM'
   },
   plugins: [
     new CleanWebpackPlugin()
