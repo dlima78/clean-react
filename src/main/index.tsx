eb139e8883b0eebd92838ba5db2cc05a0473cab1
Eduardo Lima <dlima78@hotmail.com>
2020-11-19 10:20:44 -0300

    chore: change jest to ignore index with imports files

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Router from '@/presentation/components/router/router'
+import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 
