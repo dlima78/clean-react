a01484d345e3a9033ef165d84146cd916caa126c
Eduardo Lima <dlima78@hotmail.com>
2020-11-23 08:31:12 -0300

    test: ensure Signup prevent submit if form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -151,4 +151,11 @@ describe('Signup component', () => {
     await simulateValidSubmit(sut)
     expect(addAccountSpy.callsCount).toBe(1)
   })
+
+  test('should not call Authentication if form is invalid', async () => {
+    const validationError = faker.random.words()
+    const { sut, addAccountSpy } = makeSut({ validationError })
+    await simulateValidSubmit(sut)
+    expect(addAccountSpy.callsCount).toBe(0)
+  })
 })
