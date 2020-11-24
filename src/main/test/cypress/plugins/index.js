17df7ad2db7adfa1e4b88f2d44bef3e4c95b6459
Eduardo Lima <dlima78@hotmail.com>
2020-11-24 16:48:59 -0300

    fix: fix bug

--- src/main/test/cypress/plugins/index.js
+++ src/main/test/cypress/plugins/index.js
@@ -1,5 +1,11 @@
 const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 
 module.exports = (on) => {
-  on('file:preprocessor', webpackPreprocessor())
+  const options = {
+    // send in the options from your webpack.config.js, so it works the same
+    // as your app's code
+    webpackOptions: require('../../../../../webpack.config'),
+    watchOptions: {}
+  }
+  on('file:preprocessor', webpackPreprocessor(options))
 }
